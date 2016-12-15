7ff9a2393c3699c2cd7714926329db67c6765d3a
Miguel Duarte <miguel.duarte20@hotmail.com>
2016-12-15 18:39:08 +0000

    Removed commented code from shaders

--- Engine/Engine/res/shaders/normalmapping/frag.glsl
+++ Engine/Engine/res/shaders/normalmapping/frag.glsl
@@ -1,10 +1,5 @@
 #version 330
 
-/*in vec2 ex_Texcoord;
-in vec3 ex_Normal;
-in vec3 eyeDir;
-in vec3 lightDir;*/
-
 in VS_OUT {
     vec3 FragPos;
     vec2 TexCoords;
@@ -20,41 +15,7 @@ uniform sampler2D normalTex;
 out vec4 out_Color;
 
 void main(void){
-	/*vec3 n = normalize(texture(normalTex, ex_Texcoord).rgb * 2.0 - 1.0);
-	out_Color = vec4(n, 1);
-
-	vec3 lightColor = vec3(1.0f, 1.0f, 1.0f);
-
-	vec3 E = normalize(eyeDir);
-	//vec3 N = normalize(ex_Normal);
-	vec3 N = normalize(texture(normalTex, ex_Texcoord).rgb*2.0 - 1.0); // Normal tangentspace
-	vec3 L = normalize(lightDir);
-	//L = normalize(L);
-	
-	vec3 H = normalize(E + L);
-
-	//Ambient
-	float ambientStrength = 0.5f;
-    vec3 ambient = ambientStrength * lightColor;
-
-	//diffuse
-	float diff = max(dot(L, N), 0.0);
-	vec3 diffuse = diff * lightColor;
-
-	//specular
-	float specularStrength = 0.8f;
-	float spec = pow(max(dot(H, N), 0.0), 16);
-	vec3 specular = specularStrength * spec * lightColor; 
-
-	//out_Color = ex_Color;
-	out_Color = (vec4(ambient, 1.0f) + vec4(diffuse, 1.0f) + vec4(specular, 1.0f)) * texture(tex, ex_Texcoord);*/
-	//out_Color = (vec4(diffuse, 1.0f)) * texture(tex, ex_Texcoord);
-	//float s = max(dot(L, N), 0.0f);
-	//out_Color = vec4(s, s, s, 1);
-	//out_Color = vec4(N, 1);
-
-	// Obtain normal from normal map in range [0,1]
-    vec3 normal = texture(normalTex, fs_in.TexCoords).rgb;
+	vec3 normal = texture(normalTex, fs_in.TexCoords).rgb;
     // Transform normal vector to range [-1,1]
     normal = normalize(normal * 2.0 - 1.0);  // this normal is in tangent space
     // Get diffuse color
--- Engine/Engine/res/shaders/normalmapping/vertex.glsl
+++ Engine/Engine/res/shaders/normalmapping/vertex.glsl
@@ -1,4 +1,3 @@
-
 #version 330
 
 in vec4 inPosition;
@@ -6,11 +5,6 @@ in vec2 inTexcoord;
 in vec3 inNormal;
 in vec3 inTangent;
 in vec3 inBitangent;
-
-/*out vec2 ex_Texcoord;
-out vec3 eyeDir;
-out vec3 lightDir;
-out vec3 position;*/
 
 out VS_OUT {
     vec3 FragPos;
@@ -29,33 +23,6 @@ uniform CameraBlock{
 uniform mat4 Matrix;
 
 void main(void){
-	/*vec3 normalViewSpace = inNormal;
-	vec3 tangentViewSpace = inTangent;
-	vec3 bitangentViewSpace = inBitangent;
-	mat4 TBN = mat4(transpose(mat3(
-		tangentViewSpace,
-		bitangentViewSpace,
-		normalViewSpace
-	)));
-
-	ex_Normal = inNormal;
-	
-
-	ex_Texcoord = inTexcoord;
-	mat4 ModelView = Matrix * ViewMatrix;
-
-	mat4 normalMatrix = transpose(inverse(ModelView));
-	ex_Normal = (normalMatrix * vec4(inNormal, 0.0f)).xyz;
-
-	vec3 vertexInCamSpace = (TBN * ModelView * in_Position).xyz;
-	eyeDir = -vertexInCamSpace.xyz;
-	eyeDir = normalize(eyeDir);
-
-	vec3 lightInCamSpace = (TBN * ModelView * vec4(lightPos, 1.0f)).xyz;
-	lightDir = vec3(lightInCamSpace - vertexInCamSpace).xyz;
-	lightDir = normalize(lightDir);
-	
-	gl_Position = position = ProjMatrix * ViewMatrix * Matrix * in_Position;*/
 	vec3 lightPos = vec3(0.0f, -2.0f, 2.0f);
 
 	gl_Position = ProjMatrix * ViewMatrix * Matrix * inPosition;
@@ -67,11 +34,9 @@ void main(void){
     vec3 B = normalize(mat3(Matrix) * inBitangent);
     vec3 N = normalize(mat3(Matrix) * inNormal);
     
-    //vec3 viewPos = vec3(0, 0, 0);
     mat3 TBN = transpose(mat3(T, B, N));
-    vs_out.TangentLightPos = TBN * lightPos; // multiply by TBN
-    vs_out.TangentViewPos  = TBN * CameraPosition; // multiply by TBN
-    vs_out.TangentFragPos  = TBN * vs_out.FragPos; // multiply by TBN
+    vs_out.TangentLightPos = TBN * lightPos;
+    vs_out.TangentViewPos  = TBN * CameraPosition;
+    vs_out.TangentFragPos  = TBN * vs_out.FragPos;
 
 }
-
