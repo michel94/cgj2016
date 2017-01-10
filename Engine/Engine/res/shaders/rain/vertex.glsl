65c713734c082a88194c368391aaf362860d44a3
Tiago <tiagoalmeida.92@hotmail.com>
2017-01-10 22:01:15 +0000

    submit missing files

--- Engine/Engine/res/shaders/rain/vertex.glsl
+++ Engine/Engine/res/shaders/rain/vertex.glsl
@@ -9,6 +9,7 @@ out vec2 ex_Texcoord;
 uniform CameraBlock{
 	mat4 ProjMatrix;
 	mat4 ViewMatrix;
+	vec3 CameraPosition;
 };
 
 uniform mat4 Matrix;
@@ -16,5 +17,26 @@ uniform vec4 Color;
 
 void main(void){
 	ex_Texcoord = inTexcoord;
-	gl_Position = ProjMatrix * ViewMatrix * Matrix * in_Position;
+
+	mat4 MatrixCopy = Matrix;
+
+	float x = MatrixCopy.x;
+	float y = MatrixCopy.y;
+	float z = MatrixCopy.z;
+
+//	float d = sqrt( x * x * x + y * x * x + z * x * x);
+
+//	MatrixCopy[0][0] = d;
+	//MatrixCopy[0][1] = 0;
+//	MatrixCopy[0][2] = 0;
+	
+	//MatrixCopy[1][0] = 0;
+	//MatrixCopy[1][1] = d;
+	//MatrixCopy[1][2] = 0;
+
+	//MatrixCopy[2][0] = 0;
+	//MatrixCopy[2][1] = 0;
+	//MatrixCopy[2][2] = d;
+
+	gl_Position = ProjMatrix * ViewMatrix * MatrixCopy * in_Position;
 }
